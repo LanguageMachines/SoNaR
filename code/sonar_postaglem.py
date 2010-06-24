6175c4df04dbbca531c13819a79d974a0f528e75
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-06-24 20:24:20 +0000

    added condition
    git-svn-id: https://svn.uvt.nl/ilk/trunk/sources/SoNaR@5235 12f355fe-0486-481a-ad91-c297ab22b4e3

--- code/sonar_postaglem.py
+++ code/sonar_postaglem.py
@@ -16,32 +16,29 @@ if len(sys.argv) == 2 and os.path.isdir(sys.argv[1]):
 else:
     print >>sys.stderr,"Usage: ./sonar_postaglem.py [sonar-root-directory]"
 
-for doc in CorpusX(sonardir,'tok'): #read the *.tok files
+for doc in CorpusX(sonardir,'tok',"", lambda f: not os.path.exists(f + '.pos') ): #read the *.tok files, on condition there are no *.pos equivalents
     processed_doc = False
-    if not os.path.exists(doc.filename+'.pos'):
-        print doc.filename + '\tPROCESSING'
-        for sentence in doc.sentences():
-                words = " ".join([ x.text for x in sentence ])
+    print doc.filename + '\tPROCESSING'
+    for sentence in doc.sentences():
+            words = " ".join([ x.text for x in sentence ])
 
-                process_sentence = False
-                for x in sentence:
-                    if not ns('dcoi') + 'pos' in x.attrib or not ns('dcoi') + 'lemma' in x.attrib:
-                        process_sentence = True
-                if process_sentence:
-                    processed_doc = True
-                    for i, (word, lemma, morph, pos) in enumerate(tadpoleclient.process(words)):
-                        try:
-                            word_id = sentence[i].attrib[ns('xml') + 'id']
-                        except: 
-                            print >>sys.stderr, "ERROR: words out of sync in " + sentence.attrib[ns('xml') + 'id']
-                            break
-                        if pos:
-                            doc[word_id].attrib[ns('dcoi') + 'pos'] = pos
-                        if lemma:
-                            doc[word_id].attrib[ns('dcoi') + 'lemma'] = lemma
-        if processed_doc:
-            doc.save(doc.filename+'.pos') #write .tok.pos files
-    else:
-        print doc.filename + '\tSKIPPING'
+            process_sentence = False
+            for x in sentence:
+                if not ns('dcoi') + 'pos' in x.attrib or not ns('dcoi') + 'lemma' in x.attrib:
+                    process_sentence = True
+            if process_sentence:
+                processed_doc = True
+                for i, (word, lemma, morph, pos) in enumerate(tadpoleclient.process(words)):
+                    try:
+                        word_id = sentence[i].attrib[ns('xml') + 'id']
+                    except: 
+                        print >>sys.stderr, "ERROR: words out of sync in " + sentence.attrib[ns('xml') + 'id']
+                        break
+                    if pos:
+                        doc[word_id].attrib[ns('dcoi') + 'pos'] = pos
+                    if lemma:
+                        doc[word_id].attrib[ns('dcoi') + 'lemma'] = lemma
+    if processed_doc:
+        doc.save(doc.filename+'.pos') #write .tok.pos files
 
 
