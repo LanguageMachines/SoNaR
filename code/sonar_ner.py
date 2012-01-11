9725cd75c9600af1c428dd8b27be7104ed4d776f
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-01-11 10:42:35 +0000

    Updated sonar_ner to be give a summary at the end, and added option for specifying output dir instead of in-place editing
    git-svn-id: https://svn.uvt.nl/ilk/trunk/sources/SoNaR@13947 12f355fe-0486-481a-ad91-c297ab22b4e3

--- code/sonar_ner.py
+++ code/sonar_ner.py
@@ -16,17 +16,25 @@ NERDDIR = '/exp2/NERD/nerd_voor_sonar/'
 try:
     sonardir = sys.argv[1]    
 except:
-    print >>sys.stderr, "Usage: sonar_ner.py sonardir-or-single-document-file [#processes]"
-    print >>sys.stderr, "Reads FoLiA XML, runs NERD for each document, and integrates the results"
+    print >>sys.stderr, "Usage: sonar_ner.py sonardir-or-single-document-file [#processes] [output-dir]"
+    print >>sys.stderr, "Reads FoLiA XML, runs NERD for each document, and integrates the results. If output-dir is not specified, files will be edited in place"
     sys.exit(2)
+        
     
 try:
     threads = int(sys.argv[2])    
 except:
     threads = 1
 
+
+try:
+    outputdir = sys.argv[3]    
+except:
+    outputdir = None
+
+
 def process(data):
-    global foliadir, indexlength, TMPDIR
+    global foliadir, indexlength, TMPDIR, outputdir
     filepath, args, kwargs = data
     s =  "[" +  datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '] Processing ' + filepath
     print >>sys.stderr, s        
@@ -36,10 +44,10 @@ def process(data):
         doc = folia.Document(file=filepath)
         if doc.declared(folia.AnnotationType.ENTITY, 'sonar-ner'):
             print >>sys.stderr, "WARNING: Document " + filepath +  " is already NER tagged, skipping"
-            return
+            return 2 
     except:
         print >>sys.stderr, "ERROR: Error whilst reading FoLiA Document " + filepath
-        return        
+        return 1  
         
     
     #Prepare NER input
@@ -61,7 +69,7 @@ def process(data):
     r = os.system(NERDDIR + 'nerd_for_sonar.py -i ' + tmpfile + ' -e utf-8')
     if r != 0:
         print >>sys.stderr, "ERROR: NERD failed with exit code " + str(r) + " (" + filepath + ")"
-        return
+        return 1
             
     #Read NER Output and integrate in FoLiA        
     doc.declare(folia.AnnotationType.ENTITY,'sonar-ner')
@@ -69,7 +77,7 @@ def process(data):
     
     if not os.path.exists(tmpfile):
         print >>sys.stderr, "ERROR: Expected output file " + tmpfile + ".ner not found! Error in NERD system? (" + filepath + ")"
-        return
+        return 1
     else:
         iobclass = None
         f = codecs.open(tmpfile + '.ner', 'r','utf-8')                
@@ -117,7 +125,10 @@ def process(data):
                     
         f.close()
         
-        doc.save()
+        if outputdir:
+            doc.save(outputdir + '/' + os.path.basename(doc.filename))
+        else:
+            doc.save()
 
         try:
             os.unlink(tmpfile)
@@ -127,6 +138,7 @@ def process(data):
             print >>sys.stderr, "Warning: cleanup failed (" + filepath + ")"
             
         s =  "[" +  datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '] Saved ' + filepath
+        return 0
 
 if os.path.isfile(sonardir):
     process( (sonardir,[],{}) )
@@ -134,11 +146,23 @@ elif os.path.isdir(sonardir):
     maxtasksperchild = 25
     preindex = True
 
+    processed = errors = skipped = 0
+
     processor = folia.CorpusProcessor(sonardir, process, threads, 'folia.xml', "",lambda x: True, maxtasksperchild,preindex)
     for output in processor.run():
-        if output: 
-            print output
+        if output == 0: 
+            processed += 1
+        elif output == 1:
+            errors += 1
+        elif output == 2:
+            skipped += 1
+
+    print >>sys.stderr,"All done..."
+    print >>sys.stderr,"Succesfully processed files:    " + str(processed)
+    print >>sys.stderr,"Unprocessed due to errors:      " + str(errors)
+    print >>sys.stderr,"Skipped files (already tagged): " + str(skipped)
 
 else:
     print >>sys.stderr, "Invalid SoNaR directory"
+
 
