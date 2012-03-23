6bf8d3b86dab5b80d7ee3391ea489b28e20f406f
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-03-23 10:12:31 +0000

    more robust
    git-svn-id: https://svn.uvt.nl/ilk/trunk/sources/SoNaR@14506 12f355fe-0486-481a-ad91-c297ab22b4e3

--- code/sonar_postproc.py
+++ code/sonar_postproc.py
@@ -17,94 +17,101 @@ import codecs
 
 def process(data):
     global foliadir, indexlength, inputdir, outputdir
-
+    curate = True
     filepath, args, kwargs = data
     outputfile = filepath.replace(inputdir, outputdir)
     if os.path.exists(outputfile):
-        print >>sys.stderr, "Skipping " + filepath + " (output file already exists)"
-        return None,None,None,None
+        print >>sys.stderr, "Skipping curation of " + filepath + " (output file already exists)"
+        curate = False
             
     s =  datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' ' +  filepath     
     print >>sys.stderr, s
 
 
-    #CURATION STEP
-    replace = [
-        ('<pos-annotation annotator="tadpole" annotatortype="auto" set="http://ilk.uvt.nl/folia/sets/cgn"/>', '<pos-annotation annotator="frog" annotatortype="auto" set="hdl:1839/00-SCHM-0000-0000-000B-9"/>'),
-        ('<lemma-annotation annotator="tadpole" annotatortype="auto" set="http://ilk.uvt.nl/folia/sets/lemmas-nl"/>', '<lemma-annotation annotator="frog" annotatortype="auto" set="hdl:1839/00-SCHM-0000-0000-000E-3"/>'),        
-        ('<entity-annotation set="sonar-ner"/>','<entity-annotation annotator="NERD" annotatortype="auto" set="hdl:1839/00-SCHM-0000-0000-000D-5"/>')
-    ]
-    
 
-    
-    
-    f = codecs.open(filepath,'r','utf-8')
-    outputlines = []
-    gapinsertpoint = 0
-    hasgap = False
-    metadata = 0
-    for i, line in enumerate(f):
-        if metadata == 0 and line.find('<?xml version') != -1:
-            outputlines.append(line)
-            outputlines.append('<?xml-stylesheet type="text/xsl" href="sonar-foliaviewer.xsl"?>\n')
-            continue        
-        if metadata == 0 and line.find('<metadata src=') != -1: 
-            metadata = 1
-        if metadata == 1:
-            if line.find('</metadata>') != -1:
-                gapinsertpoint = i -1
-                metadata = 2
-            else:
-                for source,target in replace:                    
-                    line = line.replace(source,target)
-        if metadata == 2:      
-            if not hasgap and line.find('<gap ') != -1: 
-                hasgap = True
-
-
-        outputlines.append(line)            
-    f.close()
-    
-    if hasgap and gapinsertpoint > 0:        
-        outputlines.insert(gapinsertpoint, '<gap-annotation />\n')
-
-    
-    dir = os.path.dirname(outputfile)
-    if not os.path.isdir(dir):
-        os.mkdir( os.path.dirname(outputfile))
-    
-    tmpfile = filepath.replace(inputdir, outputdir) + '.tmp'
-    f = codecs.open(tmpfile,'w','utf-8')
-    for line in outputlines:
-        f.write(line)
-    f.close()
-    
-    
-
+    if curate:
+        #CURATION STEP
+        replace = [
+            ('<pos-annotation annotator="tadpole" annotatortype="auto" set="http://ilk.uvt.nl/folia/sets/cgn"/>', '<pos-annotation annotator="frog" annotatortype="auto" set="hdl:1839/00-SCHM-0000-0000-000B-9"/>'),
+            ('<lemma-annotation annotator="tadpole" annotatortype="auto" set="http://ilk.uvt.nl/folia/sets/lemmas-nl"/>', '<lemma-annotation annotator="frog" annotatortype="auto" set="hdl:1839/00-SCHM-0000-0000-000E-3"/>'),        
+            ('<entity-annotation set="sonar-ner"/>','<entity-annotation annotator="NERD" annotatortype="auto" set="hdl:1839/00-SCHM-0000-0000-000D-5"/>')
+        ]
         
+        
+        try:
+            f = codecs.open(filepath,'r','utf-8')
+            outputlines = []
+            gapinsertpoint = 0
+            hasgap = False
+            metadata = 0
+            for i, line in enumerate(f):
+                if metadata == 0 and line.find('<?xml version') != -1:
+                    outputlines.append(line)
+                    outputlines.append('<?xml-stylesheet type="text/xsl" href="sonar-foliaviewer.xsl"?>\n')
+                    continue        
+                if metadata == 0 and line.find('<metadata src=') != -1: 
+                    metadata = 1
+                if metadata == 1:
+                    if line.find('</metadata>') != -1:
+                        gapinsertpoint = i -1
+                        metadata = 2
+                    else:
+                        for source,target in replace:                    
+                            line = line.replace(source,target)
+                if metadata == 2:      
+                    if not hasgap and line.find('<gap ') != -1: 
+                        hasgap = True
 
-    try:
-        os.rename(tmpfile, outputfile )
-    except:
-        print >>sys.stderr,"Unable to write file " + outputfile 
+
+                outputlines.append(line)            
+            f.close()
+        
+            if hasgap and gapinsertpoint > 0:        
+                outputlines.insert(gapinsertpoint, '<gap-annotation />\n')
+                
+                
+
+            dir = os.path.dirname(outputfile)
+            if not os.path.isdir(dir):
+                os.mkdir( os.path.dirname(outputfile))
+            
+            tmpfile = filepath.replace(inputdir, outputdir) + '.tmp'
+            f = codecs.open(tmpfile,'w','utf-8')
+            for line in outputlines:
+                f.write(line)
+            f.close()                
+            
+            try:
+                os.rename(tmpfile, outputfile )
+            except:
+                print >>sys.stderr,"Unable to write file " + outputfile 
+
+        except Exception as e:
+            print >>sys.stderr,"ERROR: Got exception curating " + filepath + ": ", repr(e)
+
     
-    #COUNT STEP
-    
+    #COUNT STEP    
     freqlist_word = FrequencyList()
     freqlist_lemma = FrequencyList()
     freqlist_lemmapos = FrequencyList()
     
-    for word in folia.Reader(filepath, folia.Word):
-        try:
-            freqlist_word.count(word.text())
-        except folia.NoSuchText:
-            print >>sys.stderr, "ERROR: Got NoSuchText error on " + word.id + " !!!"
-            continue
-        if word.lemma():
-            freqlist_lemma.count(word.lemma())
-            if word.pos():
-                freqlist_lemmapos.count( (word.lemma(), word.pos()) )
-    
+    try:
+        for word in folia.Reader(filepath, folia.Word):
+            try:
+                freqlist_word.count(word.text())
+            except folia.NoSuchText:
+                print >>sys.stderr, "ERROR: Got NoSuchText error on " + word.id + " !!!"
+                continue
+            try:
+                if word.lemma():
+                    freqlist_lemma.count(word.lemma())
+                    if word.pos():
+                        freqlist_lemmapos.count( (word.lemma(), word.pos()) )
+            except folia.NoSuchAnnotation:
+                print >>sys.stderr, "ERROR: Got NoSuchAnnotation error on " + word.id + " !!!"
+                continue
+    except Exception as e:
+        print >>sys.stderr,"ERROR: Got exception counting " + filepath + ": ", repr(e)        
     return filepath, freqlist_word, freqlist_lemma, freqlist_lemmapos
             
             
